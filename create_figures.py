9f937ca27cbaec6f2b5e53c7e1f8bc530b644f17
wilfr <wilfredvancasteren@hotmail.com>
2023-11-05 19:56:13 +0100

    features

--- create_figures.py
+++ create_figures.py
@@ -1,7 +1,9 @@
 import seaborn as sns
 import pandas as pd
 from matplotlib import pyplot as plt
-from functions import test_classifiers_dir, figures_dir
+from functions import test_classifiers_dir, figures_dir, feature_reduction_dir
+from prepare_data import prepare_data_for_specific_attack_cat
+from math import ceil
 
 
 def read_results():
@@ -35,7 +37,7 @@ def create_results_plot(results, attack_cat):
 
 def correlated_features(raw_data):
     corr_mat = raw_data.corr(method='pearson')
-    plt.figure(figsize=(25, 25))
+    plt.figure(figsize=(15, 15))
     sns.heatmap(corr_mat, square=True, annot=True, annot_kws={'size': 8})
     plt.savefig(figures_dir() + '/heatmap_features.png')
 
@@ -44,4 +46,30 @@ def correlated_features(raw_data):
         for j in range(i + 1, corr_mat.shape[0]):
             if corr_mat.iloc[i, j] >= 0.9:
                 print(f"{columns[i]:20s} {columns[j]:20s} {corr_mat.iloc[i, j]}")
-    #plt.show()
+    # plt.show()
+
+
+def pairplot(raw_data, attack_cat):
+    """
+    https://www.datacamp.com/tutorial/tutorial-lasso-ridge-regression?utm_source=google&utm_medium=paid_search&utm_campaignid=19589720818&utm_adgroupid=157156373751&utm_device=c&utm_keyword=&utm_matchtype=&utm_network=g&utm_adpostion=&utm_creative=676354848902&utm_targetid=dsa-2218886984100&utm_loc_interest_ms=&utm_loc_physical_ms=1001071&utm_content=&utm_campaign=230119_1-sea~dsa~tofu_2-b2c_3-eu_4-prc_5-na_6-na_7-le_8-pdsh-go_9-na_10-na_11-na-oct23&gclid=CjwKCAjw7oeqBhBwEiwALyHLM6I2VIxuXzBANx3jIYuJcKTrj0bCip6PCFS0GDmdnnftoJCZyGrINBoC13MQAvD_BwE
+    https://www.shedloadofcode.com/blog/eight-ways-to-perform-feature-selection-with-scikit-learn
+    """
+    attack_cat_data = prepare_data_for_specific_attack_cat(raw_data, attack_cat, 10000)
+    print(attack_cat_data.shape)
+    attack_cat_data.drop(['Label'], inplace=True, axis=1)
+    len(attack_cat_data.columns)
+    cols = attack_cat_data.columns.to_list()
+    print(len(cols))
+    for i in range(ceil(len(attack_cat_data.columns) / 4)):
+        print(i)
+        print(len(cols))
+        if len(cols) >= 4:
+            cols_to_be_shown = cols[:4]
+        elif len(cols) < 4 and len(cols) > 0:
+            cols_to_be_shown = cols
+        elif len(cols) == 0:
+            return
+
+        cols = [item for item in cols if item not in cols_to_be_shown]
+        sns.pairplot(attack_cat_data[cols_to_be_shown])
+        plt.savefig(feature_reduction_dir() + '/columns_pairplot_' + str(i) + '.png')
